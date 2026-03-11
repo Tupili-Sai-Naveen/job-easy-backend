61250a38e371bfe131224e2ef9c806fdb0e8b3ef
Tupili-Sai-Naveen <saiking123454@gmail.com>
2026-03-11 11:54:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const http = require("http");            // ← NEW
-const { Server } = require("socket.io"); // ← NEW
+const { Server } = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);   // ← NEW (wrap express)
